e65bc43734761e15bce5e3354d221f296fffa296
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-27 11:59:24 +0900

    use singleton patern

---
+++ src/main.py
@@ -0,0 +1,61 @@
+import pygame as pg
+import serial
+
+window_config = {"size":(800,600), "fps":60}
+serial_config = {"port":"COM5", "boundrate":115200}
+dualshock_config = {}
+
+class Controller:
+    def __init__(self, window_info={"":""}, serial_info={"":""}, controller_info={"":""}):
+        self.window_info = window_info
+        self.serial_info = serial_info
+        self.controller_info = controller_info
+    
+    def main(self):
+        self.setup()
+        while True:
+            self.loop()
+            for event in pg.event.get():
+                if self.controller_info and event.type in self.controller_info.get("keys"):
+                    self.getinput()
+    
+    def setup(self):
+        if self.window_info:
+            self.setWindow()
+        if self.serial_info:
+            self.beginSerial()
+        
+    def loop(self):
+        if self.window_info:
+           self.updateWindow()
+        if self.serial_info:
+            self.sendSerial() 
+    
+
+    def setWindow(self):
+        ...
+    
+    def updateWindow(self):
+        ...
+        
+
+    def beginSerial(self):
+        ...
+    
+    def sendSerial(self):
+        ...
+        
+
+    def watchValue(self):
+        ...
+
+    def getEvent(self):
+        ...
+
+if __name__ == "__main__":
+    obj = Controller(
+        window_info = window_config,
+        serial_info = serial_config,
+        controller_info = dualshock_config
+    )
+    obj.main()
