6be77d8953068de72d1fb70ca45c611c8f063879
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-29 00:19:04 +0900

    completed

--- src/GamepadInput.py
+++ src/GamepadInput.py
@@ -5,41 +5,57 @@ import json
 class GamePad:
     def __init__(self, config_json, print_log = False):
         self.print_log = print_log
-        #C:\Users\ka_po\Documents\Python_2026\raspi-controller\src\config\DualShock4.json
         with open(os.path.join(os.getcwd(),"raspi-controller","src","config", config_json), "r") as config:
             self.gamepad_info = json.load(config)
         self.gamepad = joystick.Joystick(0)
-        self.states = {}#key:None for key in self.gamepad_info["Button"]}
-        self.values = {}#key:None for key in self.gamepad_info["Axis"]}
+        self.states = {}
+        self.values = {}
     
     def onButtonDown(self, button):
-        if self.print_log: print(self.gamepad.info["Button"][button])
-        
+        if self.print_log: print(self.gamepad_info["Button"][button])
+        #if self.gamepad_info["Button"][button] == "Button12":
+            #print(self.values)
         
     def onAxisMove(self, axis, value):
-        if self.print_log: print(self.gamepad.info["Axis"][axis],value)
+        if self.print_log: print(self.gamepad_info["Axis"][axis],value)
         
+    def onHatTilt(self, hat, value):
+        if self.print_log: print(hat,value)
     
     def update(self):
-        self.states = {button:self.gamepad.get_button(button) for button in self.gamepad_info["Button"]}
-        self.values = {axis:self.gamepad.get_button(axis) for axis in self.gamepad_info["Axis"]}
+        self.states = {button:self.gamepad.get_button(n) for n,button in enumerate(self.gamepad_info["Button"])}
+        for n,hat in enumerate(self.gamepad_info.get("Hat",[])):
+            value = self.gamepad.get_hat(n)
+            self.states[hat[0]] = value[1] == 1
+            self.states[hat[1]] = value[1] ==-1
+            self.states[hat[2]] = value[0] == 1
+            self.states[hat[3]] = value[0] ==-1
+        self.states
+        self.values = {axis:self.gamepad.get_axis(n) for n,axis in enumerate(self.gamepad_info["Axis"])}
         if self.print_log:
-            print(self.states)
-            print(self.values)
+            #print(self.states)
+            #print(self.values)
+            pass
 
 import pygame
 
 def main():
     pygame.init()
-    pad=GamePad("DualShock4.json",True)
+    pad=GamePad("ElecomPad.json",True)#DualShock4
     pygame.display.set_mode((300,300))
     while True:
         pad.update()
         for event in pygame.event.get():
             if event.type == pygame.JOYAXISMOTION:
+                #print(event.axis, event.value)
                 pad.onAxisMove(event.axis, event.value)
             if event.type == pygame.JOYBUTTONDOWN:
+                #print(event.button)
                 pad.onButtonDown(event.button)
+            if event.type == pygame.JOYHATMOTION:
+                if event.value[0]+event.value[1]:
+                    pad.onHatTilt(event.hat, event.value)
+            
         pygame.display.update()
         
 if __name__ == "__main__":
