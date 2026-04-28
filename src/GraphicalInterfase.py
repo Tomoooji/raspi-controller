18cdc4d5bb83dd61acf117e0d4191b97d12d3510
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-28 14:56:54 +0900

    prototype

--- src/GraphicalInterfase.py
+++ src/GraphicalInterfase.py
@@ -7,20 +7,21 @@ class GraphicInterfase:
         with open(path.join("config",config_json), "r") as config:
             self.window_info = json.load(config)
         self.screen = None
+        
     
     def begin(self):
         self.screen = pygame.display.set_mode(self.window_info["size"])
         if "title" in self.window_info:
             pygame.dispay.set_caption(self.window_info["title"])
     
-    def drawFlame(self):
+    def draw(self):
         if "rect" in self.window_info:
             for args in self.window_info["rect"]:
                 rect = pygame.Rect(0,0,args["width"],args["heighr"])
                 rect.center = (args["center_x"], args["center_y"])
                 pygame.draw.rect(
                     self.screen,
-                    self.window_info["color"][0],
+                    self.window_info["color"]["active" if args["plessed"] else "base"],
                     rect,
                     5
                 )
@@ -29,7 +30,7 @@ class GraphicInterfase:
             for args in self.window_info["circle"]:
                 pygame.draw.circle(
                     self.screen,
-                    self.window_info["color"][0],
+                    self.window_info["color"]["active" if args["plessed"] else "base"],
                     (args["center_x"], args["center_y"]),
                     args["radius"]
                 )
@@ -38,32 +39,54 @@ class GraphicInterfase:
             for args in self.window_info["stick"]:
                 pygame.draw.circle(
                     self.screen,
-                    self.window_info["color"][1]
+                    self.window_info["color"]["base"]
                     (args["center_x"],args["center_y"]),
                     args["out_radius"],
                     5
                 )
+                pygame.draw.circle(
+                    self.screen,
+                    self.window_info["color"]["active" if args["plessed"] else "base"],
+                    (args["value_x"]*args["out_ardius"]+args["center_x"], args["value_y"]*args["out_ardius"]+args["center_y"]),
+                    args["in_radius"]
+                )
 
         if "bar_h" in self.window_info:
             for args in self.window_info["bar"]:
-                pygame.draw.circle(
+                pygame.draw.line(
                     self.screen,
-                    self.window_info["color"][1],
-                    ((args["width"]-args["height"])/2 + args["center_x"], args["center_y"]),
-                    args["width"]/2
+                    self.window_info["color"]["active" if "plessed" in args and args["plessed"] else "base"],
+                    (-args["width"]/2+args["center_x"],args["center_y"]),
+                    (+args["width"]/2+args["center_x"],args["center_y"]),
+                    args["height"]
                 )
-                pygame.draw.circle(
-                    self.screen,
-                    self.window_info["color"][1],
-                    ((args["width"]-args["height"])/2 - args["center_x"], args["center_y"]),
-                    args["width"]/2
-                )
-                rect = pygame.Rect(0,0,args["width"]-args["height"],args["height"])
-                rect.center = (args["center_x"], args["center_y"])
-                pygame.draw.rect(
-                    self.screen,
-                    self.window_info["color"][1],
-                    rect
-                )
-    
-    
+                if "value" in args:
+                    pygame.draw.line(
+                        self.screen,
+                        self.window_info["color"]["active"],
+                        (-args["width"]/2+args["center_x"], args["center_y"]),
+                        ((args["value"]*args["width"]/2)+args["center_x"], args["center_y"]),
+                        args["height"]-2
+                    )
+                # lineだと端が四角だがaalineは太さを指定できない
+                # 両端は〇の方が良いかもしれない
+                #pygame.draw.circle(
+                #    self.screen,
+                #    self.window_info["color"][1],
+                #    ((args["width"]-args["height"])/2 + args["center_x"], args["center_y"]),
+                #    args["width"]/2
+                #)
+                #pygame.draw.circle(
+                #    self.screen,
+                #    self.window_info["color"][1],
+                #    ((args["width"]-args["height"])/2 - args["center_x"], args["center_y"]),
+                #    args["width"]/2
+                #)
+                # rectじゃなくてここだけlineにしてもよいかも
+                #rect = pygame.Rect(0,0,args["width"]-args["height"],args["height"])
+                #rect.center = (args["center_x"], args["center_y"])
+                #pygame.draw.rect(
+                #    self.screen,
+                #    self.window_info["color"][1],
+                #    rect
+                #)
